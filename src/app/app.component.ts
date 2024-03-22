7b0d392a84ca5594e96fe8a081e3d62b1e8edef3
Triet Tran <tranvuminhtriet@gmail.com>
2024-03-22 14:53:02 +0700

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ElementRef, ViewChild } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'poc_fetch_s3-imgs';
+  @ViewChild("imageRef") imageRef!: ElementRef<any>
+
+  inputImgSrc = "/assets/IMG_vis.jpeg"
+
+  imgToShow: any
+
+  constructor() {}
+
+  onImageLoad(): void {
+    console.log("Image loaded successfully!")
+    var canvas = document.createElement("canvas")
+    const ctx = canvas.getContext("2d")
+    if (ctx) {
+      ctx.drawImage(this.imageRef.nativeElement, canvas.width, canvas.height)
+      ctx.getImageData(0, 0, canvas.width, canvas.height)
+    }
+  }
+
+  applyLayer(e: any) {}
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core"
+import { BrowserModule } from "@angular/platform-browser"
+import { RouterOutlet } from "@angular/router"
+import { AppComponent } from "./app.component"
+import { ImageService } from "./image.service"
+import { HttpClientModule } from "@angular/common/http"
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterOutlet, HttpClientModule],
+  providers: [ImageService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
