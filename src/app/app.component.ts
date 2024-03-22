ab476352cf206153dafe606431f1901871167ca3
Triet Tran <tranvuminhtriet@gmail.com>
2024-03-22 15:12:12 +0700

    Add scale function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
 
   onImageLoad(): void {
     console.log("Image loaded successfully!")
+
     var canvas = document.createElement("canvas")
     canvas.width = this.imageRef.nativeElement.naturalWidth
     canvas.height = this.imageRef.nativeElement.width
@@ -24,7 +25,37 @@ export class AppComponent {
       ctx.drawImage(this.imageRef.nativeElement, 0, 0, canvas.width, canvas.height)
       const dataInputImage = ctx.getImageData(0, 0, canvas.width, canvas.height)
       console.log(dataInputImage)
+
+      this.scaleImage(canvas)
+      const scaledImage = ctx.getImageData(0, 0, canvas.width, canvas.height)
+      console.log(scaledImage)
     }
+  }
+
+  scaleImage(canvas: HTMLCanvasElement) {
+    const sizeW = 800 // HD 1920x1080
+    const sizeH = 600 // HD 1920x1080
+
+    const scaleFactor = this.imageRef.nativeElement.width / this.imageRef.nativeElement.height
+    // Set actual size in memory (scaled to account for extra pixel density).
+    if (scaleFactor >= 1) {
+      // width >= height
+      canvas.width = Math.floor(
+        this.imageRef.nativeElement.width > sizeW ? sizeW : this.imageRef.nativeElement.naturalWidth
+      )
+      canvas.height = Math.floor(
+        this.imageRef.nativeElement.width > sizeW ? sizeW / scaleFactor : this.imageRef.nativeElement.naturalHeight
+      )
+    } else {
+      // width < height
+      canvas.width = Math.floor(
+        this.imageRef.nativeElement.height > sizeH ? sizeH * scaleFactor : this.imageRef.nativeElement.naturalWidth
+      )
+      canvas.height = Math.floor(
+        this.imageRef.nativeElement.height > sizeH ? sizeH : this.imageRef.nativeElement.naturalHeight
+      )
+    }
+    return canvas
   }
 
   applyLayer(e: any) {}
