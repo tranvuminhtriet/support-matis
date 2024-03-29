c98be1b4fb9976f2a8e2b920b994448ea212b64a
Triet Tran <tranvuminhtriet@gmail.com>
2024-03-29 10:37:29 +0700

    latest update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from "./app.component"
 import { ImageWithLayerComponent } from "./image-with-layer/image-with-layer.component"
 import { ImageApplyLayerComponent } from "./image-apply-layer/image-apply-layer.component"
+import { FormsModule } from "@angular/forms"
 
 @NgModule({
   declarations: [AppComponent, ImageWithLayerComponent, ImageApplyLayerComponent],
-  imports: [BrowserModule, RouterOutlet, HttpClientModule],
+  imports: [BrowserModule, RouterOutlet, HttpClientModule, FormsModule],
   providers: [ImageService],
   bootstrap: [AppComponent],
 })
--- src/app/image-with-layer/image-with-layer.component.ts
+++ src/app/image-with-layer/image-with-layer.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core"
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core"
 import { MockedInputType } from "./types/MockedInputType"
 
 @Component({
@@ -9,39 +9,88 @@ import { MockedInputType } from "./types/MockedInputType"
 export class ImageWithLayerComponent implements AfterViewInit {
   @ViewChild("canvas") canvasRef!: ElementRef<HTMLCanvasElement>
 
+  layers: { colorName?: string; opacity: number; image: HTMLImageElement }[] = []
+  selectedColor = []
+
   inputData: MockedInputType = [
-    { colorName: "i1", colorHex: "#aa0e26", src: "/assets/i1.png" },
-    { colorName: "i2", colorHex: "#f80e80", src: "/assets/i2.png" },
-    { colorName: "i3", colorHex: "#3434ff", src: "/assets/i3.png" },
+    { colorName: "i1", colorHex: "#a90c22", src: "/assets/i1.png" },
+    { colorName: "i2", colorHex: "#fa99ff", src: "/assets/i2.png" },
+    { colorName: "i3", colorHex: "#c86477", src: "/assets/i3.png" },
+    { colorName: "i4", colorHex: "#3333ff", src: "/assets/i4.png" },
+    { colorName: "i5", colorHex: "#6464ff", src: "/assets/i5.png" },
+    { colorName: "i6", colorHex: "#fa9934", src: "/assets/i6.png" },
+    { colorName: "i7", colorHex: "#073300", src: "/assets/i7.png" },
+    { colorName: "i8", colorHex: "#36cc00", src: "/assets/i8.png" },
+    { colorName: "i9", colorHex: "#280128", src: "/assets/i9.png" },
+    { colorName: "i10", colorHex: "#cccc01", src: "/assets/i10.png" },
+    { colorName: "i11", colorHex: "#0113cc", src: "/assets/i11.png" },
+    { colorName: "i12", colorHex: "#f80e7f", src: "/assets/i12.png" },
+    { colorName: "i13", colorHex: "#990200", src: "/assets/i13.png" },
+    { colorName: "i14", colorHex: "#000566", src: "/assets/i14.png" },
+    { colorName: "i15", colorHex: "#282828", src: "/assets/i15.png" },
   ]
 
   ngAfterViewInit(): void {
-    const newImg = new Image()
-    newImg.src = this.inputData[0].src
-    this.canvasRef.nativeElement.width = newImg.width
-    this.canvasRef.nativeElement.height = newImg.height
-    newImg.src = this.inputData[1].src
-    newImg.src = this.inputData[2].src
+    this.generateCanvasSize()
   }
 
-  drawLayer(n?: string) {
-    let context = this.canvasRef.nativeElement.getContext("2d")
+  generateCanvasSize() {
+    const canvas = this.canvasRef.nativeElement
+    const image = new Image()
+    image.onload = () => {
+      canvas.width = image.width
+      canvas.height = image.height
+    }
+    image.src = this.inputData[0].src
+  }
+
+  drawLayer(i: number) {
+    const canvas = this.canvasRef.nativeElement
+    const context = canvas.getContext("2d")
     if (context) {
       const newImg = new Image()
-      switch (n) {
-        case "i1":
-          newImg.src = this.inputData[0].src
-          break
-        case "i2":
-          newImg.src = this.inputData[1].src
-          break
-        case "i3":
-          newImg.src = this.inputData[2].src
-          break
-        default:
-          break
+      newImg.src = this.inputData[i].src
+      newImg.onload = () => {
+        this.layers.push({ colorName: this.inputData[i].colorName, image: newImg, opacity: 1 })
+        this.drawAllLayers()
       }
-      context.drawImage(newImg, 0, 0)
+    }
+  }
+
+  layerExists(colorName?: string) {
+    return this.layers.some((layer) => layer.colorName === colorName)
+  }
+
+  removeLayer(colorName?: string) {
+    const index = this.layers.findIndex((layer) => layer.colorName === colorName)
+    if (index !== -1) {
+      this.layers.splice(index, 1)
+      this.drawAllLayers()
+    }
+  }
+
+  getOpacity(colorName?: string): number {
+    const layer = this.layers.find((layer) => layer.colorName === colorName)
+    return layer ? layer.opacity : 1 // default opacity is 1
+  }
+
+  updateOpacity(opacity: string, colorName?: string): void {
+    const layerIndex = this.layers.findIndex((layer) => layer.colorName === colorName)
+    if (layerIndex !== -1) {
+      this.layers[layerIndex].opacity = parseFloat(opacity)
+      this.drawAllLayers()
+    }
+  }
+
+  drawAllLayers() {
+    const canvas = this.canvasRef.nativeElement
+    const context = canvas.getContext("2d")
+    if (context) {
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      for (const layer of this.layers) {
+        context.globalAlpha = layer.opacity // Set opacity for each layer
+        context.drawImage(layer.image, 0, 0)
+      }
     }
   }
 }
