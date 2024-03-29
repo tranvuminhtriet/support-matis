263cab64bd6b6bb74918e6ede5769f8010e3fe2e
Triet Tran <tranvuminhtriet@gmail.com>
2024-03-29 11:29:35 +0700

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
+import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
 import { BrowserModule } from "@angular/platform-browser"
 import { RouterOutlet } from "@angular/router"
-import { ImageService } from "./image.service"
-import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from "./app.component"
 import { ImageWithLayerComponent } from "./image-with-layer/image-with-layer.component"
-import { ImageApplyLayerComponent } from "./image-apply-layer/image-apply-layer.component"
-import { FormsModule } from "@angular/forms"
+import { ImageService } from "./image.service"
 
 @NgModule({
-  declarations: [AppComponent, ImageWithLayerComponent, ImageApplyLayerComponent],
+  declarations: [AppComponent, ImageWithLayerComponent],
   imports: [BrowserModule, RouterOutlet, HttpClientModule, FormsModule],
   providers: [ImageService],
   bootstrap: [AppComponent],
